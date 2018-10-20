83debaaed550594e7c9c713431f98e01c94af11e
harneet singh <harneet.singh.ts@gmail.com>
2018-10-20 23:12:53 +0530

    Minor changes

--- AppPortal/MasterPage.Master.designer.cs
+++ AppPortal/MasterPage.Master.designer.cs
@@ -38,5 +38,23 @@ namespace AppPortal {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// email_txt control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email_txt;
+        
+        /// <summary>
+        /// pwd_txt control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox pwd_txt;
     }
 }
